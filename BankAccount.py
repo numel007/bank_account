a3a4808059c7612493268d7b0619558371f3feaf
Jackie <taekwondo.chan@gmail.com>
2020-10-27 18:08:15 -0700

    add users

--- BankAccount.py
+++ BankAccount.py
@@ -12,6 +12,7 @@ class BankAccount:
         self.balance += amount
         print(f"Amount Deposited: ${amount}")
         print(f"New balance: ${self.balance}")
+        print("\n")
         return self.balance
 
     def withdraw(self, amount):
@@ -21,13 +22,16 @@ class BankAccount:
             print(f"Insufficient funds to withdraw ${amount}")
             print(f"Overcharge fee: $10")
             print(f"New balance: ${self.balance}")
+            print("\n")
         else:
             self.balance -= amount
             print(f"Amount Withdrawn: ${amount}")
             print(f"New Balance: ${self.balance}")
+            print("\n")
 
     def get_balance(self):
         print(f"You currently have an account balance of: ${self.balance}")
+        print("\n")
         return self.balance
 
     def add_interest(self):
@@ -36,22 +40,36 @@ class BankAccount:
         self.balance += interest
         print(f"Interest: ${interest}")
         print(f"New balance: ${self.balance}")
+        print("\n")
 
     def print_receipt(self):
         account_num_to_string = str(self.account_number)
 
-        print(f"\n {self.full_name}")
+        print(f"{self.full_name}")
         print(f"Account number: ****{account_num_to_string[-4:]}")
         print(f"Routing number: {self.routing_number}")
         print(f"Balance: ${self.balance}")
+        print("\n")
     
 
 person1 = BankAccount("Jackie", 11111111, 1111111111, 100) 
 person2 = BankAccount("Ben", 22222222, 22222222, 200)
 person3 = BankAccount("Jeremiah", 33333333, 33333333, 300)
 
-# person1.deposit(int(input("Input deposit amount: ")))
-# person1.withdraw(int(input("Input withdrawal amount: ")))
-# person1.get_balance()
-# person1.add_interest()
+person1.deposit(int(input(f"Hello {person1.full_name}. Input deposit amount: $")))
+person1.withdraw(int(input(f" Hello {person1.full_name}. Input withdrawal amount: $")))
+person1.get_balance()
+person1.add_interest()
 person1.print_receipt()
+
+person2.deposit(int(input(f"Hello {person2.full_name}. Input deposit amount: $")))
+person2.withdraw(int(input(f" Hello {person2.full_name}. Input withdrawal amount: $")))
+person2.get_balance()
+person2.add_interest()
+person2.print_receipt()
+
+person3.deposit(int(input(f"Hello {person3.full_name}. Input deposit amount: $")))
+person3.withdraw(int(input(f" Hello {person3.full_name}. Input withdrawal amount: $")))
+person3.get_balance()
+person3.add_interest()
+person3.print_receipt()
