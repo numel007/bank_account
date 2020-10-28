732216fb925ee956411b6f2ae65554f22a21de4d
Jackie <taekwondo.chan@gmail.com>
2020-10-27 18:01:30 -0700

    add print_receipt method

--- BankAccount.py
+++ BankAccount.py
@@ -37,14 +37,21 @@ class BankAccount:
         print(f"Interest: ${interest}")
         print(f"New balance: ${self.balance}")
 
-    # def print_receipt(self):
+    def print_receipt(self):
+        account_num_to_string = str(self.account_number)
+
+        print(f"\n {self.full_name}")
+        print(f"Account number: ****{account_num_to_string[-4:]}")
+        print(f"Routing number: {self.routing_number}")
+        print(f"Balance: ${self.balance}")
     
 
-person1 = BankAccount("Jackie", 1, 11, 100) 
-person2 = BankAccount("Ben", 2, 22, 200)
-person3 = BankAccount("Jeremiah", 3, 33, 300)
+person1 = BankAccount("Jackie", 11111111, 1111111111, 100) 
+person2 = BankAccount("Ben", 22222222, 22222222, 200)
+person3 = BankAccount("Jeremiah", 33333333, 33333333, 300)
 
-person1.deposit(int(input("Input deposit amount: ")))
-person1.withdraw(int(input("Input withdrawal amount: ")))
-person1.get_balance()
-person1.add_interest()
+# person1.deposit(int(input("Input deposit amount: ")))
+# person1.withdraw(int(input("Input withdrawal amount: ")))
+# person1.get_balance()
+# person1.add_interest()
+person1.print_receipt()
