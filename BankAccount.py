995f3284e59e8f7654f40b4edf43af40dff5cdec
Jackie <taekwondo.chan@gmail.com>
2020-10-27 17:33:44 -0700

    created BankAccount class and some empty methods

---
+++ BankAccount.py
@@ -0,0 +1,23 @@
+class BankAccount:
+
+    def __init__(self, full_name, account_number, routing_number, balance):
+    self.full_name = full_name
+    self.account_number = account_number
+    self.routing_number = routing_number
+    self.balance = balance
+
+
+    def deposit(self, amount):
+
+    def withdraw(self, amount):
+
+    def get_balance(self):
+
+    def add_interest(self, balance):
+
+    def print_receipt(self):
+    
+
+person1 = BankAccount("Jackie", 1, 11, 100) 
+person2 = BankAccount("Ben",2, 22, 200 )
+person3 = BankAccount("Jeremiah", 3, 33, 300)
