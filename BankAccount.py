f3f68b310c525b643244e58c0be640df11a2ca41
Jackie <taekwondo.chan@gmail.com>
2020-11-01 17:25:10 -0800

    added basic atm functions

--- BankAccount.py
+++ BankAccount.py
@@ -5,22 +5,23 @@ class BankAccount:
         self.full_name = full_name
         self.account_number = account_number
         self.routing_number = routing_number
-        self.balance = balance
+        self.balance = 0
 
+    # ------------------ Methods ------------------
 
-    #-------- Methods --------
-    
-    # Deposit user-defined amount
     def deposit(self, amount):
+        """Deposit user-defined amount"""
         print(f"Current balance: ${self.balance}")
         self.balance += amount
         print(f"Amount Deposited: ${amount}")
+        self.balance -= atmUsageFee
+        print(f"ATM fee: ${atmUsageFee}")
         print(f"New balance: ${self.balance}")
         print("\n")
         return self.balance
 
-    # Withdraw user-defined amount or subtract $10 on overdraft
     def withdraw(self, amount):
+        """Withdraw user-defined amount or subtract $10 on overdraft"""
         print(f"Current balance: ${self.balance}")
         if amount > self.balance:
             self.balance -= 10
@@ -31,17 +32,19 @@ class BankAccount:
         else:
             self.balance -= amount
             print(f"Amount Withdrawn: ${amount}")
+            self.balance -= atmUsageFee
+            print(f"ATM fee: ${atmUsageFee}")
             print(f"New Balance: ${self.balance}")
             print("\n")
 
-    # Returns user's current balance
     def get_balance(self):
+        """Returns user's current balance"""
         print(f"You currently have an account balance of: ${self.balance}")
         print("\n")
         return self.balance
 
-    # Adds 1 month's interest to user's balance, calculated from current balance at rate of 0.00083
     def add_interest(self):
+        """Adds 1 month's interest to user's balance, calculated from current balance at rate of 0.00083"""
         print(f"Current balance: ${self.balance}")
         interest = self.balance * 0.00083
         self.balance += interest
@@ -49,41 +52,148 @@ class BankAccount:
         print(f"New balance: ${self.balance}")
         print("\n")
 
-    # Print's user's account information, censors first 4 digits of acc. number
     def print_receipt(self):
+        """Print's user's account information, censors first 4 digits of acc. number"""
         account_num_to_string = str(self.account_number)
 
-        print(f"{self.full_name}")
+        print(f"Account Holder: {self.full_name}")
         print(f"Account number: ****{account_num_to_string[-4:]}")
         print(f"Routing number: {self.routing_number}")
         print(f"Balance: ${self.balance}")
         print("\n")
-    
 
-# -------- Testing methods --------
+# ------------------ ATM Functions ------------------
+
+def deposit(user, route_num):
+    """Selects from list of known users and runs deposit method"""
+    while True:
+        if user == Tom.full_name and route_num == Tom.account_number:
+            Tom.deposit(int(input(f"Hello {Tom.full_name}. Input deposit amount: $")))
+            break
+        elif user == Bob.full_name and route_num == Bob.account_number:
+            Bob.deposit(int(input(f"Hello {Bob.full_name}. Input deposit amount: $")))
+            break
+        elif user == Hubert.full_name and route_num == Hubert.account_number:
+            Hubert.deposit(int(input(f"Hello {Hubert.full_name}. Input deposit amount: $")))
+            break
+        else:
+            print("No matching user found")
+            continue
+    print("Exiting ATM")
+
+def withdraw(user, route_num):
+    """Selects from list of known users and runs withdrawal method"""
+    while True:
+        if user == Tom.full_name and route_num == Tom.account_number:
+            Tom.withdraw(int(input(f"Hello {Tom.full_name}. Input withdrawal amount: $")))
+            break
+        elif user == Bob.full_name and route_num == Bob.account_number:
+            Bob.withdraw(int(input(f"Hello {Bob.full_name}. Input withdrawal amount: $")))
+            break
+        elif user == Hubert.full_name and route_num == Hubert.account_number:
+            Hubert.withdraw(int(input(f"Hello {Hubert.full_name}. Input withdrawal amount: $")))
+            break
+        else:
+            print("No matching user found")
+            continue
+    print("Exiting ATM")
+
+def balance(user, route_num):
+    """Selects from list of known users and runs get_balance method"""
+    while True:
+        if user == Tom.full_name and route_num == Tom.account_number:
+            Tom.get_balance()
+            break
+        elif user == Bob.full_name and route_num == Bob.account_number:
+            Bob.get_balance()
+            break
+        elif user == Hubert.full_name and route_num == Hubert.account_number:
+            Hubert.get_balance()
+            break
+        else:
+            print("No matching user found")
+            continue
+    print("Exiting ATM")
+
+def accDetails(user, route_num):
+    """Selects from list of known users and runs get_balance method"""
+    while True:
+        if user == Tom.full_name and route_num == Tom.account_number:
+            Tom.print_receipt()
+            break
+        elif user == Bob.full_name and route_num == Bob.account_number:
+            Bob.print_receipt()
+            break
+        elif user == Hubert.full_name and route_num == Hubert.account_number:
+            Hubert.print_receipt()
+            break
+        else:
+            print("No matching user found")
+            continue
+    print("Exiting ATM")
+
+def atmOptions():
+    while True:
+        option_selected = int(input("Enter an option\n\n1) Deposit\n2) Withdraw\n3) Current Balance\n4) Account Details\n\nOption selected: "))
+        if option_selected == 1:
+            print("Deposit selected")
+            print("--------------------\n")
+            deposit(input("Input name: "), int(input("Input account number: ")))
+            break
+        elif option_selected == 2:
+            print("Withdrawal selected")
+            print("--------------------\n")
+            withdraw(input("Input name: "), int(input("Input account number: ")))
+            break
+        elif option_selected == 3:
+            print("Current Balance selected")
+            print("--------------------\n")
+            balance(input("Input name: "), int(input("Input account number: ")))
+            break
+        elif option_selected == 4:
+            print("Account details selected")
+            print("--------------------\n")
+            accDetails(input("Input name: "), int(input("Input account number: ")))
+            break
+        else:
+            print("Invalid option")
+            print("--------------------\n")
+            continue
+
+
+# ------------------ Testing methods ------------------
 
 # Initialize 3 users with predefined BankAccount variables
-person1 = BankAccount("Jackie", 11111111, 1111111111, 100) 
-person2 = BankAccount("Ben", 22222222, 22222222, 200)
-person3 = BankAccount("Jeremiah", 33333333, 33333333, 300)
+Tom = BankAccount("Tom", 11111111, 1111111111, 0)
+Bob = BankAccount("Bob", 22222222, 22222222, 2)
+Hubert = BankAccount("Hubert Blaine Wolfeschlegel­steinhausen­bergerdorff­vor­altern­waren­gewissenhaft­schafers­wessen­schafts­waren­wohl­gefuttern­und­sorgfaltigkeit­beschutzen­vor­angreifen­durch­ihr­raubgierig­fiends Sr.", 33333333, 33333333, 300)
+atmUsageFee = 2
+
+# Give each account a starting balance, so test methods and functions can be called
+Tom.balance = 100
+Bob.balance = 200
+Hubert.balance = 300
 
 # Person1 method calls
-person1.deposit(int(input(f"Hello {person1.full_name}. Input deposit amount: $")))
-person1.withdraw(int(input(f" Hello {person1.full_name}. Input withdrawal amount: $")))
-person1.get_balance()
-person1.add_interest()
-person1.print_receipt()
+# Tom.deposit(int(input(f"Hello {Tom.full_name}. Input deposit amount: $")))
+# Tom.withdraw(int(input(f"Hello {Tom.full_name}. Input withdrawal amount: $")))
+# Tom.get_balance()
+# Tom.add_interest()
+# Tom.print_receipt()
 
 # Person2 method calls
-person2.deposit(int(input(f"Hello {person2.full_name}. Input deposit amount: $")))
-person2.withdraw(int(input(f" Hello {person2.full_name}. Input withdrawal amount: $")))
-person2.get_balance()
-person2.add_interest()
-person2.print_receipt()
+# Bob.deposit(int(input(f"Hello {Bob.full_name}. Input deposit amount: $")))
+# Bob.withdraw(int(input(f"Hello {Bob.full_name}. Input withdrawal amount: $")))
+# Bob.get_balance()
+# Bob.add_interest()
+# Bob.print_receipt()
 
 # Person3 method calls
-person3.deposit(int(input(f"Hello {person3.full_name}. Input deposit amount: $")))
-person3.withdraw(int(input(f" Hello {person3.full_name}. Input withdrawal amount: $")))
-person3.get_balance()
-person3.add_interest()
-person3.print_receipt()
+# Hubert.deposit(int(input(f"Hello {Hubert.full_name}. Input deposit amount: $")))
+# Hubert.withdraw(int(input(f"Hello {Hubert.full_name}. Input withdrawal amount: $")))
+# Hubert.get_balance()
+# Hubert.add_interest()
+# Hubert.print_receipt()
+
+# chooseUser(input("Input name: "), int(input("Input account number: ")))
+atmOptions()
